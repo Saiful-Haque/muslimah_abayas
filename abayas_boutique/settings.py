9b1d23048fba1b41c4ff945463ab9d53a7b16d63
Muslimah Abayas <shopmuslimahabayas@gmail.com>
2026-06-07 07:12:20 +0530

    Fix media and static URLs with leading slash to load images on collection catalog

--- abayas_boutique/settings.py
+++ abayas_boutique/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'store' / 'static',
 ]
@@ -132,7 +132,7 @@ STORAGES = {
     },
 }
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
